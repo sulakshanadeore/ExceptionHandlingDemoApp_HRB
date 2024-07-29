4a6e45a209309437f22168abef0262c3cbc7789d
deoresulakshana <deoresulakshana@gmail.com>
2024-07-29 12:39:15 +0530

    nested try

---
+++ ExceptionHandlingDemoApp/NestedTry.cs
@@ -0,0 +1,69 @@
+﻿using ShoppingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemoApp
+{
+    internal class NestedTry
+    {
+        static void Main(string[] args)
+        {
+            UserLogin login = new UserLogin();
+            try
+            {
+                Console.WriteLine("Enter username");
+                login.UserName = Console.ReadLine().Trim();
+                Console.WriteLine("Enter Password");
+                login.Password = Console.ReadLine().Trim();
+                login.Initialize();
+                bool ans=login.ValidateUser(login.UserName,login.Password);
+                Console.WriteLine("Login Successful");
+                Console.ReadLine();
+                Console.Clear();
+                if (ans) {
+                    try
+                    {
+
+                        Console.WriteLine("Enter Customer id");
+                        int custid = 1;
+                        Console.WriteLine("Enter Name");
+                        string? name = null;
+
+                        Customer c=new Customer(custid,name);
+
+                        CustomerOperations custop = new CustomerOperations();
+                        bool custStatus=custop.AddCustomer(c);
+                        if (custStatus)
+                        {
+                            List<Customer> custs=custop.GetCustomers();
+                            foreach (var item in custs)
+                            {
+                                Console.WriteLine(item.CustID);
+                                Console.WriteLine(item.Name);
+                            }
+                        }
+
+                    }
+                    catch (DivideByZeroException zeroEx)
+                    {
+                        //inner
+                        Console.WriteLine(zeroEx.Message);
+                    }
+                
+                
+                
+                }
+
+            }
+            catch (ArgumentNullException arEx)
+            {
+                //outer
+                Console.WriteLine(arEx.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- ShoppingLibrary/CustomerOperations.cs
+++ ShoppingLibrary/CustomerOperations.cs
@@ -6,10 +6,91 @@ using System.Threading.Tasks;
 
 namespace ShoppingLibrary
 {
+    public class UserLogin 
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public static List<UserLogin> list = new List<UserLogin>();
+
+
+        public void Initialize()
+        {
+            list.Add(new UserLogin {UserName="Jack",Password="Jack@123" });
+            list.Add(new UserLogin { UserName = "Jim", Password = "Jim@123" });
+            list.Add(new UserLogin { UserName = "Jill", Password = "Jill@123" });
+
+        }
+
+        public bool ValidateUser(string username,string password)
+        {
+            bool LoginStatus = false;
+            UserLogin user=list.Find(u => u.UserName == username);
+            if (user != null)
+            {
+                if (user.Password == password)
+                { 
+                LoginStatus = true; 
+                
+                }
+
+
+            }
+            return LoginStatus; 
+        }
+    
+    }
+
+
+
+    public class Customer {
+        public int CustID{ get;  set; }
+        public string Name { get;  set; }
+
+        public Customer()
+        {
+                
+        }
+
+        public Customer( int cid,string cname)
+        {
+            CustID = cid;
+            Name = cname;   
+        }
+
+    }
     public class CustomerOperations
     {
 
-        public void FindCustomer(int id)
+        static List<Customer> custList = new List<Customer>();
+
+        public CustomerOperations()
+        {
+            custList.Add(new Customer {CustID=2,Name="Ajinkya" });
+        }
+
+
+        public List<Customer> GetCustomers() {
+        return custList;
+        
+        }
+
+        public bool AddCustomer(Customer customer)
+        {
+            bool status = false;
+            if (customer.Name != null)
+            {
+                custList.Add(customer);
+                status = true;
+            }
+            else
+            {
+                throw new ArgumentNullException("Customer data not found");
+            }
+        return status;
+        }
+
+         public void FindCustomer(int id)
         {
             if (id <= 0)
             {
